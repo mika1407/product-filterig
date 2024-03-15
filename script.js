146232c20d5ae96c4b7bbac13d9b06fd6428fdd9
mika tiihonen <koneautomaatio@hotmail.com>
2024-03-15 19:58:33 +0200

    Loop Over Products & Create Elements

--- script.js
+++ script.js
@@ -1,96 +1,113 @@
 const products = [
   {
     name: 'Sony Playstation 5',
-    url: 'images/playstation-5.png',
+    url: 'images/playstation_5.png',
     category: 'games',
     price: 499.99,
   },
   {
     name: 'Samsung Galaxy',
-    url: 'images/samsung-galaxy.png',
+    url: 'images/samsung_galaxy.png',
     category: 'smartphones',
     price: 399.99,
   },
   {
     name: 'Cannon EOS Camera',
-    url: 'images/cannon-eos-camera.png',
+    url: 'images/cannon_eos_camera.png',
     category: 'cameras',
     price: 749.99,
   },
   {
     name: 'Sony A7 Camera',
-    url: 'images/sony-a7-camera.png',
+    url: 'images/sony_a7_camera.png',
     category: 'cameras',
     price: 1999.99,
   },
   {
     name: 'LG TV',
-    url: 'images/QHgFnHk/lg-tv.png',
+    url: 'images/lg_tv.png',
     category: 'televisions',
     price: 799.99,
   },
   {
     name: 'Nintendo Switch',
-    url: 'images/nintendo-switch.png',
+    url: 'images/nintendo_switch.png',
     category: 'games',
     price: 299.99,
   },
   {
     name: 'Xbox Series X',
-    url: 'images/xbox-series-x.png',
+    url: 'images/xbox_series_x.png',
     category: 'games',
     price: 499.99,
   },
   {
     name: 'Samsung TV',
-    url: 'images/samsung-tv.png',
+    url: 'images/samsung_tv.png',
     category: 'televisions',
     price: 1099.99,
   },
   {
     name: 'Google Pixel',
-    url: 'images/google-pixel.png',
+    url: 'images/google_pixel.png',
     category: 'smartphones',
     price: 499.99,
   },
   {
     name: 'Sony ZV1F Camera',
-    url: 'images/sony-zv1f-camera.png',
+    url: 'images/sony_zv1f_camera.png',
     category: 'cameras',
     price: 799.99,
   },
   {
     name: 'Toshiba TV',
-    url: 'images/toshiba-tv.png',
+    url: 'images/toshiba_tv.png',
     category: 'televisions',
     price: 499.99,
   },
   {
     name: 'iPhone 14',
-    url: 'images/iphone-14.png',
+    url: 'images/iphone_14.png',
     category: 'smartphones',
     price: 999.99,
   },
 ];
 
 // Get DOM elements
-const productsWrapperEl = document.getElementById('products-wrapper');
-const checkEls = document.querySelectorAll('.check');
+const productsWrapper = document.getElementById('products-wrapper');
+const checkboxes = document.querySelectorAll('.check');
 const filtersContainer = document.getElementById('filters-container');
 const searchInput = document.getElementById('search');
-const cartButton = document.getElementById('cartButton');
-const cartCount = document.getElementById('cartCount');
+//const cartButton = document.getElementById('cartButton');
+const cartCount = document.getElementById('cart-count');
 
 // Initialize cart item count
 let cartItemCount = 0;
 
 // Initialize products
-const productsEls = [];
+const productElements = [];
 
 // Loop over the products and create the product elements
 products.forEach((product) => {
-  // const productEl = createProductElement(product);
-  // productsEls.push(productEl);
-  // productsWrapperEl.appendChild(productEl);
-  console.log(product)
+  const productElement = document.createElement('div');
+   productElement.className = 'item space-y-2';
+   
+    productElement.innerHTML = `<div
+    class="bg-gray-100 flex justify-center relative overflow-hidden group cursor-pointer border rounded-xl"
+  >
+    <img
+      src="${product.url}"
+      alt="${product.name}"
+      class="w-full h-full object-cover"
+    />
+    <span
+      class="status bg-black text-white absolute bottom-0 left-0 right-0 text-center py-2 translate-y-full transition group-hover:translate-y-0"
+      >Add To Cart</span
+    >
+  </div>
+  <p class="text-xl">${product.name}</p>
+  <strong>€${product.price.toLocaleString()}</strong>`;
+
+  productElements.push(productElement);
+  productsWrapper.appendChild(productElement);
 });
